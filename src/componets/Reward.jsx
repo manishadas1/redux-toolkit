a89b04ec0e439305e33822649b0eabde4e052888
manishadas1 <manishadas.akrabhi@gmail.com>
2023-10-20 19:20:58 +0530

    redux-toolkits

---
+++ src/componets/Reward.jsx
@@ -0,0 +1,28 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { increment, incrementByAmount } from "../reducers/reward";
+
+
+function Reward(){
+
+
+    const dispatch = useDispatch();
+   const points = useSelector(state=>state.reward.points);
+
+
+    return (
+        <div className="card">
+            <div className="container">
+            <h4>
+                <b>Reward Component</b>
+            </h4>
+            <h3>Total Points : {points}</h3> 
+         <button onClick={()=>dispatch(increment())}>Increment + </button>
+         <button onClick={()=>dispatch(incrementByAmount(7))}>IncrementBySeven + </button>
+            </div>
+        </div>
+    );
+
+}
+
+export default Reward;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { configureStore } from '@reduxjs/toolkit';
+import accountReducer from './slices/accountSlice';
+import bonusReducer from './slices/bonusSlice';
+import { Provider } from 'react-redux';
+import rewardReducer from './reducers/reward';
+
+
+
+const store = configureStore({
+  reducer:{
+account: accountReducer,
+bonus: bonusReducer,
+reward: rewardReducer
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
